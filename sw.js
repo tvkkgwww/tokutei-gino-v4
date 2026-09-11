ce4d13c1c36db1db9154e5f1014201ad0ebd1bac
tvkkgwww <tvkkgwww@gmail.com>
2026-09-11 23:20:45 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='tg2-v4.3';const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='tg2-v4.4-20260911';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
